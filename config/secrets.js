3884b51a76022e220116e624fb4b23657bc900af
Maksim Pecherskiy <max@maksimize.com>
2014-06-16 09:00:24 -0700

    Changing secrets default.

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/loveplatform',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/michael-loveplatform',
 
   localAuth: true,
   sessionSecret: "Your Session Secret goes here",
